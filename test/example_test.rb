84d307b632ac170a91872d355b61799d331c248e
Nick Quaranto <nick@quaran.to>
2011-12-23 22:09:12 -0500

    it works, kinda

---
+++ test/example_test.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+
+class ExampleTest < Test::Unit::TestCase
+  def setup
+  end
+
+  def test_apple
+    assert_equal 1, 1
+  end
+
+  def test_banana
+    assert_equal 1, 1
+  end
+end
+
+#class Wtf
+#  def zomg
+#
+#  end
+#
+#  def lol(*args)
+#
+#  end
+#
+#end
