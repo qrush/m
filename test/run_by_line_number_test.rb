ac9bf3b58fa04a4b4397c3eb9641704215d2d3dd
Nick Quaranto <nick@quaran.to>
2011-12-24 16:12:35 -0500

    refactor assertions

--- test/run_by_line_number_test.rb
+++ test/run_by_line_number_test.rb
@@ -3,36 +3,26 @@ require 'test_helper'
 class RunByLineNumberTest < MTest
   def test_run_simple_test_by_line_number
     output = m('test/example_test.rb:7')
-
-    assert $?.success?, "Execution failed, output:\n\n#{output}"
-    assert_match /1 tests, 1 assertions/, output
+    assert_output /1 tests, 1 assertions/, output
   end
 
   def test_runs_entire_test_without_line_number
     output = m('test/example_test.rb')
-
-    assert $?.success?, "Execution failed, output:\n\n#{output}"
-    assert_match /2 tests/, output
+    assert_output /2 tests/, output
   end
 
   def test_run_inside_of_test
     output = m('test/example_test.rb:8')
-
-    assert $?.success?, "Execution failed, output:\n\n#{output}"
-    assert_match /1 tests, 1 assertions/, output
+    assert_output /1 tests, 1 assertions/, output
   end
 
   def test_run_on_end_of_test
     output = m('test/example_test.rb:9')
-
-    assert $?.success?, "Execution failed, output:\n\n#{output}"
-    assert_match /1 tests, 1 assertions/, output
+    assert_output /1 tests, 1 assertions/, output
   end
 
   def test_run_inside_big_test
     output = m('test/example_test.rb:14')
-
-    assert $?.success?, "Execution failed, output:\n\n#{output}"
-    assert_match /1 tests, 3 assertions/, output
+    assert_output /1 tests, 3 assertions/, output
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,9 @@ class MTest < Test::Unit::TestCase
   def m(arguments)
     `ruby -Ilib ./bin/m #{arguments}`.strip
   end
+
+  def assert_output(regexp, output)
+    assert $?.success?, "Execution failed, output:\n\n#{output}"
+    assert_match regexp, output
+  end
 end
