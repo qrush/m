f2bd0d90b040a13c41321854d8df65a0d9a5a489
Nick Quaranto <nick@quaran.to>
2011-12-26 12:29:28 -0500

    extract runner to its own file

--- lib/m.rb
+++ lib/m.rb
@@ -4,57 +4,12 @@ require "ostruct"
 require "ruby_parser"
 require "sourcify"
 
+require "m/runner"
 require "m/test"
 require "m/test_collection"
 require "m/version"
 
 module M
-  class Runner
-    def initialize(argv)
-      @file, line = argv.first.split(':')
-      @line = line.to_i
-    end
-
-    def run
-      tests_to_run = tests.within(@line)
-
-      if tests_to_run.size > 0
-        test_names = tests_to_run.map(&:name).join('|')
-        exit ::Test::Unit::AutoRunner.run(false, nil, ["-n", "/(#{test_names})/"])
-      else
-        message = "No tests found on line #{@line}. Valid tests to run:\n\n"
-        tests.by_line_number do |test|
-          message << "#{sprintf("%0#{tests.column_size}s", test.name)}: m #{@file}:#{test.start_line}\n"
-        end
-        abort message
-      end
-    end
-
-    private
-
-    def suites
-      $:.unshift "./test"
-      load @file
-      ::Test::Unit::TestCase.test_suites.inject({}) do |suites, suite_class|
-        suites[suite_class] = suite_class.test_methods unless suite_class.test_methods.empty?
-        suites
-      end
-    end
-
-    def tests
-      @tests ||= begin
-        collection = M::TestCollection.new
-        suites.each do |suite_class, test_methods|
-          suite = suite_class.new(//)
-          test_methods.each do |test_method|
-            collection << M::Test.new_from_object_and_method(suite, test_method)
-          end
-        end
-        collection
-      end
-    end
-  end
-
   def self.run(argv)
     Runner.new(argv).run
   end
