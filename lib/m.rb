0103d28a931459a0b8e97fc1b7099a9a6dc325cc
Nick Quaranto <nick@quaran.to>
2012-01-12 00:35:41 -0500

    fix stupid linebreak formatting

--- lib/m.rb
+++ lib/m.rb
@@ -22,18 +22,18 @@
 #
 #Given this file:
 #
-#     $ cat -n test/example_test.rb
-#     1	require 'test/unit'
-#     2	
-#     3	class ExampleTest < Test::Unit::TestCase
-#     4	  def test_apple
-#     5	    assert_equal 1, 1
-#     6	  end
-#     7	
-#     8	  def test_banana
-#     9	    assert_equal 1, 1
-#    10	  end
-#    11	end
+#      $ cat -n test/example_test.rb
+#      1	require 'test/unit'
+#      2	
+#      3	class ExampleTest < Test::Unit::TestCase
+#      4	  def test_apple
+#      5	    assert_equal 1, 1
+#      6	  end
+#      7	
+#      8	  def test_banana
+#      9	    assert_equal 1, 1
+#     10	  end
+#     11	end
 #
 #You can run a test by line number, using format `m TEST_FILE:LINE_NUMBER_OF_TEST`:
 #
