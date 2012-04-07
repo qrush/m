da3f005303dadb3757cd9e46018dfe0c415f2bbc
Nick Quaranto <nick@quaran.to>
2012-04-07 11:35:42 -0400

    Bump to 1.1.0

--- lib/m.rb
+++ lib/m.rb
@@ -15,14 +15,14 @@
 #If you're using Bundler, you'll need to include it in your Gemfile. Toss it into the `test` group:
 #
 #     group :test do
-#       gem 'm', '~> 1.0'
+#       gem 'm', '~> 1.1'
 #     end
 #
 #Developing a RubyGem? Add `m` as a development dependency.
 #
 #     Gem::Specification.new do |gem|
 #       # ...
-#       gem.add_development_dependency "m", "~> 1.0"
+#       gem.add_development_dependency "m", "~> 1.1"
 #     end
 #
 #`m` is Ruby 1.9+ only. Sorry, but `method_source`, `sourcify`, and `ruby_parser`
@@ -98,7 +98,7 @@
 ### M, your metal test runner
 # Maybe this gem should have a longer name? Metal?
 module M
-  VERSION = "1.0.1"
+  VERSION = "1.1.0"
 
   # Accept arguments coming from bin/m and run tests.
   def self.run(argv)
