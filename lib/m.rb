a0ee6985203f5ba2c5780ec3bf446993b67ac00d
Nick Gauthier <ngauthier@gmail.com>
2013-04-19 13:47:54 -0400

    sync output before hard exit

--- lib/m.rb
+++ lib/m.rb
@@ -100,6 +100,9 @@ module M
 
   # Accept arguments coming from bin/m and run tests, then bail out immediately.
   def self.run(argv)
+    # sync output since we're going to exit hard and fast
+    $stdout.sync = true
+    $stderr.sync = true
     exit! Runner.new(argv).run
   end
 
