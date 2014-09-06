205ced30f04544d9d54c42cf90f435707b45bfd1
Teo Ljungberg <teo@teoljungberg.com>
2014-09-06 11:20:31 +0200

    Quell "ambiguous first argument" warnings

--- test/active_support_test.rb
+++ test/active_support_test.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class ActiveSupportTest < MTest
   def test_run_simple_test_by_line_number
     output = m('examples/active_support_example_test.rb:12')
-    assert_output /1 tests, 1 assertions/, output
+    assert_output(/1 tests, 1 assertions/, output)
   end
 
   def test_runs_entire_test_without_line_number
     output = m('examples/active_support_example_test.rb')
-    assert_output /4 tests/, output
+    assert_output(/4 tests/, output)
   end
 
   def test_run_inside_of_test
     output = m('examples/active_support_example_test.rb:13')
-    assert_output /1 tests, 1 assertions/, output
+    assert_output(/1 tests, 1 assertions/, output)
   end
 
   def test_run_on_end_of_test
     output = m('examples/active_support_example_test.rb:14')
-    assert_output /1 tests, 1 assertions/, output
+    assert_output(/1 tests, 1 assertions/, output)
   end
 
   def test_run_inside_big_test
     output = m('examples/active_support_example_test.rb:18')
-    assert_output /1 tests, 3 assertions/, output
+    assert_output(/1 tests, 3 assertions/, output)
   end
 
   def test_run_on_blank_line_orders_tests_by_line_number
@@ -43,11 +43,11 @@ EOF
 
   def test_run_on_test_with_spaces
     output = m('examples/active_support_example_test.rb:22')
-    assert_output /1 tests, 1 assertions/, output
+    assert_output(/1 tests, 1 assertions/, output)
   end
 
   def test_run_on_test_with_unescaped_regular_express_characters
     output = m('examples/active_support_unescaped_example_test.rb:8')
-    assert_output /1 tests, 1 assertions/, output
+    assert_output(/1 tests, 1 assertions/, output)
   end
 end
--- test/everything_test.rb
+++ test/everything_test.rb
@@ -3,26 +3,26 @@ require 'test_helper'
 class EverythingTest < MTest
   def test_runs_entire_test_suite_with_no_arguments
     output = m('')
-    assert_output /14 tests/, output
+    assert_output(/14 tests/, output)
   end
 
   def test_missing_file_gives_a_decent_error_message
     output = m('examples/thisdoesnexist_test.rb')
     assert !$?.success?
-    assert_match /Failed loading test file/, output
-    assert_match /cannot load such file/, output
+    assert_match(/Failed loading test file/, output)
+    assert_match(/cannot load such file/, output)
   end
 
   def test_running_tests_within_a_subdirectory
     output = m('examples/subdir')
-    assert_output /3 tests/, output
+    assert_output(/3 tests/, output)
 
     output = m('examples')
-    assert_output /14 tests/, output
+    assert_output(/14 tests/, output)
   end
 
   def test_blank_file_is_quieter
     output = m('bananas')
-    assert_no_match /Valid tests to run/, output
+    assert_no_match(/Valid tests to run/, output)
   end
 end
