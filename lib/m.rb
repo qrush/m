5f4f65b3c0306f5db59b0c8113cd5e273d8c62fb
Teo Ljungberg <teo@teoljungberg.com>
2014-09-06 11:21:29 +0200

    Don't shadow outer variable suites

--- lib/m.rb
+++ lib/m.rb
@@ -242,10 +242,10 @@ module M
       end
 
       # Use some janky internal APIs to group test methods by test suite.
-      suites.inject({}) do |suites, suite_class|
+      suites.inject({}) do |test_suites, suite_class|
         # End up with a hash of suite class name to an array of test methods, so we can later find them and ignore empty test suites
-        suites[suite_class] = suite_class.test_methods if suite_class.test_methods.size > 0
-        suites
+        test_suites[suite_class] = suite_class.test_methods if suite_class.test_methods.size > 0
+        test_suites
       end
     end
 
