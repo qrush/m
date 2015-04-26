5a0962045a04f69aaa372e5f66825ebc1bed8c4a
Teo Ljungberg <teo@teoljungberg.com>
2015-04-26 15:42:23 +0200

    Make `m` warning free

--- test/examples/minitest_4_example_test.rb
+++ test/examples/minitest_4_example_test.rb
@@ -22,9 +22,9 @@ if M::Frameworks.minitest4?
     end
 
     def test_that_it_will_not_blend
-      refute_match /^maybe/i, @meme.will_it_blend?
-      refute_match /^no/i, @meme.will_it_blend?
-      refute_match /^lolz/i, @meme.will_it_blend?
+      refute_match(/^maybe/i, @meme.will_it_blend?)
+      refute_match(/^no/i, @meme.will_it_blend?)
+      refute_match(/^lolz/i, @meme.will_it_blend?)
     end
 
     def test_that_kitty_can_eat_two_time
--- test/examples/minitest_5_example_test.rb
+++ test/examples/minitest_5_example_test.rb
@@ -21,9 +21,9 @@ if M::Frameworks.minitest5?
     end
 
     def test_that_it_will_not_blend
-      refute_match /^maybe/i, @meme.will_it_blend?
-      refute_match /^no/i, @meme.will_it_blend?
-      refute_match /^lolz/i, @meme.will_it_blend?
+      refute_match(/^maybe/i, @meme.will_it_blend?)
+      refute_match(/^no/i, @meme.will_it_blend?)
+      refute_match(/^lolz/i, @meme.will_it_blend?)
     end
 
     def test_that_kitty_can_eat_two_time
