422a128a9f3e243d706148f39d05bc863345749b
Mike Moore <mike@blowmage.com>
2022-12-10 13:54:39 -0700

    Remove method_source dependency
    Replace with a modified version of Rails' TestParser using Ripper.
    
    Co-authored-by: Jeremy Evans <code@jeremyevans.net>

--- lib/m/test_method.rb
+++ lib/m/test_method.rb
@@ -1,4 +1,4 @@
-require "method_source" if RUBY_VERSION < "4"
+require_relative "finish_line"  if RUBY_VERSION < "4"
 
 module M
   ### Simple data structure for what a test method contains.
@@ -9,23 +9,16 @@ module M
   # Includes the name of this method, what line on the file it begins on,
   # and where it ends.
   TestMethod = Struct.new :name, :start_line, :end_line do
-    # Set up a new test method for this test suite class
     def self.create suite_class, test_method
-      # Hopefully it's been defined as an instance method, so we'll need to
-      # look up the ruby Method instance for it
       method = suite_class.instance_method test_method
 
       # Ruby can find the starting line for us, so pull that out of the array.
       # Ruby 4.0+ can also provide the ending line.
-      start_line, end_line  = method.source_location.values_at(1, 3)
+      start_line, end_line = method.source_location.values_at(1, 3)
 
-      # Ruby < 4.0 can't find the end line however, and I'm too lazy to write
-      # a parser. Instead, `method_source` adds `Method#source` so we can
-      # deduce this ourselves.
-      #
-      # The end line should be the number of line breaks in the method source,
-      # added to the starting line and subtracted by one.
-      end_line ||= method.source.split("\n").size + start_line - 1
+      # Ruby < 4.0 can't find the end line. Use a Ripper-derived parser to
+      # determine the ending line.
+      end_line ||= FinishLine.ending_line_for method
 
       # Shove the given attributes into a new databag
       new test_method, start_line, end_line
